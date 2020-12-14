a4ee1723a4197d53bf1eefcb23196c19d91e6182
Noel <mastropierro6@gmail.com>
2020-12-14 09:53:44 -0600

    env adjusted

--- scripts/windows/run.bat
+++ scripts/windows/run.bat
@@ -2,11 +2,11 @@ echo "Declaring environment variables"
 
 rem Yous should change this two path for the correct path to the public and private keys
 
-set PRIVATE_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/serializer/jwt/key_backup/id_rsa"
-set PUBLIC_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/serializer/jwt/key_backup/id_rsa.pub"
+SET PRIVATE_KEY_PATH=E:/Kmada Proyectos/Proyectos Freelancer/Tests/construct_demo/serializer/jwt/key_backup/id_rsa
+SET PUBLIC_KEY_PATH=E:/Kmada Proyectos/Proyectos Freelancer/Tests/construct_demo/serializer/jwt/key_backup/id_rsa.pub
 
-set MONGO_URL="localhost"
+set MONGO_URL="localhost:27017"
 set REDIS_PASSW="construct_demo.321"
 set REDIS_URL="redis://default:$REDIS_PASSW@localhost:6379/0"
 
-go run ..\..\main.go
+go run ../../main.go
