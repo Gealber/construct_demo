fb57b01451819a02d17cb539f7a7e85bd36465a1
Gealber <mbasalo@nauta.cu>
2020-12-13 19:58:19 -0500

    Changing environment default for Noel majaseria celestial

--- scripts/linux/env-local.sh
+++ scripts/linux/env-local.sh
@@ -4,5 +4,5 @@ export PRIVATE_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/
 export PUBLIC_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/serializer/jwt/key_backup/id_rsa.pub"
 export MONGO_URL="localhost"
 
-export REDIS_PASSW="construct_demo.321"
-export REDIS_URL="redis://default:$REDIS_PASSW@localhost:6379/0"
+# export REDIS_PASSW="construct_demo.321"
+# export REDIS_URL="redis://default:$REDIS_PASSW@localhost:6379/0"
--- scripts/windows/run.bat
+++ scripts/windows/run.bat
@@ -2,11 +2,10 @@ echo "Declaring environment variables"
 
 rem Yous should change this two path for the correct path to the public and private keys
 
-set PRIVATE_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/serializer/jwt/key_backup/id_rsa"
-set PUBLIC_KEY_PATH="/home/gealber/go/src/github.com/Gealber/construct_demo/serializer/jwt/key_backup/id_rsa.pub"
+set PRIVATE_KEY_PATH=""
+set PUBLIC_KEY_PATH=""
 
 set MONGO_URL="localhost"
-set REDIS_PASSW="construct_demo.321"
-set REDIS_URL="redis://default:$REDIS_PASSW@localhost:6379/0"
+set REDIS_URL=""
 
 go run ..\..\main.go
